e156c0ea6d492a9f0e69c63975692edaf3fe4b06
Satyasheel Singh <satyasheel2004@gmail.com>
2026-06-16 18:19:55 +0530

    send hardcoded json to `/progress` endpoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"encoding/json"
 )
 
 func main() {
@@ -11,6 +12,16 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Write a response to w on every request to "/"
 		http.ServeFile(w, r, "roadmap.html")
+	})
+
+	http.HandleFunc("/progress", func(w http.ResponseWriter, r *http.Request) {
+		// Write a json response to w for progress
+		w.Header().Set("Content-Type", "application/json")
+
+		checks := map[string]bool{"pp5": true, "ppfx1": true}
+		if err := json.NewEncoder(w).Encode(checks); err != nil {
+			log.Println("encode progress: ", err)
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
