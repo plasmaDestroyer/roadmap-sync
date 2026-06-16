634e48ffa48289dbad31d751336a323bd00edcdc
Satyasheel Singh <satyasheel2004@gmail.com>
2026-06-16 18:07:48 +0530

    complete the minimal http server.

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Starting Server...")
+	log.Println("Starting Server...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Write a response to w on every request to "/"
 		http.ServeFile(w, r, "roadmap.html")
 	})
-	http.ListenAndServe(":8080", nil)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
